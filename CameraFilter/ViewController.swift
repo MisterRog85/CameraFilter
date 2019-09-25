848f3cb718bea7de1557eea78b3fdc1f3ba6d1e2
William Tomas <william.tomas@mobiapps.fr>
2019-09-25 15:11:42 +0200

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by William Tomas on 25/09/2019.
+//  Copyright © 2019 William Tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
